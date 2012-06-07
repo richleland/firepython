2fbfc4c88bbd16ea03da503529a032c80d89783d
Rich Leland <rich@richleland.com>
2012-06-06 22:53:08 -0400

    some pep8 cleanups

--- firepython/middleware.py
+++ firepython/middleware.py
@@ -38,6 +38,7 @@ __all__ = [
 jsonpickle.load_backend('django.utils.simplejson', 'dumps',
                         'loads', ValueError)
 
+
 class FirePythonBase(object):
 
     def __init__(self):
@@ -125,7 +126,7 @@ class FirePythonBase(object):
         return (exc_type, exc_value, exc_traceback)
 
     def _handle_internal_exception(self, e):
-        if CONST.RAZOR_MODE: # in razor mode hurt web server
+        if CONST.RAZOR_MODE:  # in razor mode hurt web server
             raise e
         # in non-razor mode report internal error to firepython addon
         exc_info = self._sanitize_exc_info(sys.exc_info())
@@ -148,10 +149,10 @@ class FirePythonBase(object):
             # __str__ implementations on various objects
             errors = [self._handle_internal_exception(e)]
             try:
-                data = jsonpickle.encode({"errors": errors },
+                data = jsonpickle.encode({"errors": errors},
                                          unpicklable=False,
                                          max_depth=CONST.JSONPICKLE_DEPTH)
-            except Exception, e:
+            except Exception:
                 # even unable to serialize error message
                 data = jsonpickle.encode(
                         {"errors": {
@@ -234,7 +235,7 @@ class FirePythonBase(object):
                 while t:
                     try:
                         d = {}
-                        for k,v in t.tb_frame.f_locals.iteritems():
+                        for k, v in t.tb_frame.f_locals.iteritems():
                             if CONST.DEEP_LOCALS:
                                 d[unicode(k)] = v
                             else:
@@ -277,8 +278,10 @@ class FirePythonBase(object):
         except ImportError:
             import profile
         self._prof = profile.Profile()
+
         def prof_wrapper(*args, **kwargs):
             return self._prof.runcall(func, *args, **kwargs)
+
         return prof_wrapper
 
     def _prepare_profile(self):
@@ -365,7 +368,6 @@ class FirePythonDjango(FirePythonBase):
         request.firepython_set_extension_data = self._extension_data.__setitem__
 
     def process_view(self, request, callback, callback_args, callback_kwargs):
-        args = (request,) + callback_args
         callback = self._profile_wrap(callback)
         return None
 
@@ -409,7 +411,7 @@ class FirePythonWSGI(FirePythonBase):
     def __call__(self, environ, start_response):
         check = self._check(environ)
         if not check and not self._client_message:
-            return self.app(environ, start_response) # a quick path
+            return self.app(environ, start_response)  # a quick path
 
         # firepython is enabled or we have a client message we want to communicate in headers
         client_message = self._client_message
@@ -418,6 +420,7 @@ class FirePythonWSGI(FirePythonBase):
         closure = ["200 OK", [], None]
         extension_data = {}  # Collect extension data here
         sio = StringIO()
+
         def faked_start_response(_status, _headers, _exc_info=None):
             closure[0] = _status
             closure[1] = _headers
@@ -462,7 +465,7 @@ class FirePythonWSGI(FirePythonBase):
 
         # start responding
         write = start_response(*closure)
-        if sio.tell(): # position is not 0
+        if sio.tell():  # position is not 0
             sio.seek(0)
             write(sio.read())
         # return output
